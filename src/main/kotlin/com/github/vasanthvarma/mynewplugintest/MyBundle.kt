43f6213bf1cd451fbd9fadffb0dd559603e6807f
GitHub Action <action@github.com>
2024-02-19 09:17:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/vasanthvarma/mynewplugintest/MyBundle.kt
+++ src/main/kotlin/com/github/vasanthvarma/mynewplugintest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.vasanthvarma.mynewplugintest
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/vasanthvarma/mynewplugintest/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/vasanthvarma/mynewplugintest/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.vasanthvarma.mynewplugintest.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.vasanthvarma.mynewplugintest.MyBundle
+import com.github.vasanthvarma.mynewplugintest.services.MyProjectService
 import javax.swing.JButton
 
 
